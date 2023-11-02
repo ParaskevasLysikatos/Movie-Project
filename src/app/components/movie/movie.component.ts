de042bfa39268106191c8dde0fe43b376ef205c7
Paraskevas <vaslysalex@hotmail.gr>
2023-11-02 10:54:46 +0200

    star rating

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -11,8 +11,6 @@ import { MovieService } from 'src/app/services/movie.service';
 })
 export class MovieComponent implements OnInit {
 
-  constructor(private movieSrv:MovieService,private router:ActivatedRoute) { }
-
 getMovieDetailResult:any;
 
 spoken_languages!: string;
@@ -21,20 +19,32 @@ guest!:Guest;
 
 rateStatus: string="Rate this movie!";
 
-@ViewChild('sliderInput')
-  slider!: HTMLInputElement;
+// @ViewChild('sliderInput')
+//   slider!: HTMLInputElement;
+stars: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+selectedValue: number=1;
+
+constructor(private movieSrv:MovieService,private router:ActivatedRoute) {
+  let getParamId = this.router.snapshot.paramMap.get('id');
+  console.log(getParamId,'getparamid#');
+
+  this.getMovie(getParamId);
+ }
 
   ngOnInit(): void {
-    let getParamId = this.router.snapshot.paramMap.get('id');
-    console.log(getParamId,'getparamid#');
 
-    this.getMovie(getParamId);
+  }
+
+  countStar(star: number) {
+    this.selectedValue = star;
+    console.log('Value of star', star);
   }
 
 getMovie(id:any){
     this.movieSrv.getMovieDetails(id).subscribe((result)=>{
         console.log(result,'getmoviedetails#');
         this.getMovieDetailResult = result;
+        this.selectedValue=this.getMovieDetailResult.vote_average;
       this.spoken_languages = result.spoken_languages.map((language: any) => language?.name).join(', ');
 
       this.movieSrv.getGuestSession().subscribe((res) => {
@@ -45,17 +55,18 @@ getMovie(id:any){
     });
   }
 
-rateForm = new FormGroup({
-    'rating':new FormControl(5)
-  });
+// rateForm = new FormGroup({
+//     'rating':new FormControl(5)
+//   });
 
   submitForm()
   {
-      console.log(this.rateForm.value,'rateform#');
-    this.movieSrv.postMovieRating(this.rateForm.controls.rating.value ?? 5, Number(this.router.snapshot.paramMap.get('id')), this.guest.guest_session_id)
+      console.log(this.selectedValue,'rateform#');
+    this.movieSrv.postMovieRating(this.selectedValue, Number(this.router.snapshot.paramMap.get('id')), this.guest.guest_session_id)
     .subscribe((res) => {
       console.log(res,'getMovieRating');
       this.rateStatus='You rated this movie. Thank you!'
+      this.getMovie(this.router.snapshot.paramMap.get('id'));
     });
   }
 
